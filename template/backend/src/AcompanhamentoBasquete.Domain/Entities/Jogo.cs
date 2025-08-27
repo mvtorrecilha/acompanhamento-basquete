3f78f2668cbe352d1f3181fd726b8671d067620b
Marcos Vinícius Bonancin Torrecilha <marcos.bonancin@confitec.com.br>
2025-08-27 14:00:46 -0300

    Adicionado camada Core -  domain

---
+++ template/backend/src/AcompanhamentoBasquete.Domain/Entities/Jogo.cs
@@ -0,0 +1,10 @@
+﻿using AcompanhamentoBasquete.Domain.Common;
+
+namespace AcompanhamentoBasquete.Domain.Entities;
+
+public class Jogo : EntidadeBase
+{
+    public DateTime Data { get; set; }
+
+    public int Pontos { get; set; }
+}
