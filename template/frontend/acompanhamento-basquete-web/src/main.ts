4d42775ab7d6d5818adf7f2993dfd1eacb4380dc
Marcos Vinícius Bonancin Torrecilha <marcos.bonancin@confitec.com.br>
2025-08-27 21:24:51 -0300

    Estrutura inicial do front end

---
+++ template/frontend/acompanhamento-basquete-web/src/main.ts
@@ -0,0 +1,16 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideRouter(routes),
+    provideHttpClient()
+  ],
+})
+.catch(err => console.error(err));
